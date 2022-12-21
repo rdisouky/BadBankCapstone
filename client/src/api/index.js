282a98bd44ecd030ff4e1398b6cb66774ce1c727
Rawan Disouky <102531594+rdisouky@users.noreply.github.com>
2022-12-21 00:07:30 -0500

    Add files via upload

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 
 import axios from 'axios';
-const URL='http://localhost:5000/';
+const URL=process.env.REACT_APP_SERVER_URL;
 const CREATE_USER = 'users/createUser';
 const DEPOSIT = 'service/deposit';
 const WITHDRAW = 'service/withdraw';
